7329cc9aa46696b827ccb1053e95f8798d2a6b68
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-21 08:41:43 +0100

    Just to test print_strings with ALX's main funct

---
+++ 0-main.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+int main(void)
+{
+	 print_strings("String:[%s]\n", "I am a string !");
+
+	 return (0);
+}
+
+
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef _HEADER_
+#define _HEADER_
+#include <stdlib.h>
+
+void print_strings(const char *mainstr, ...);
+
+#endif
