c075ffc80b19ce95a2797e2b1329080898ace44b
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-22 14:43:16 +0100

    Test your new code in this repo

--- rep_format.c
+++ rep_format.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * rep_format - This replaces specifiers with corresponding data type
 * @specifier: characters to be checked to execute conditionals
@@ -12,22 +11,26 @@ int rep_format(char specifier, va_list list)
 	int count = 0;
 	char *str;
 
+	if (specifier == '\0')
+		return (0);
 	if (specifier == 'c')
 		count += _putchar(va_arg(list, int));
 	else if (specifier == 's')
 	{
 		str = va_arg(list, char *);
+
 		while (*str)
 		{
 			count += _putchar(*str);
 			str++;
 		}
+		count += _putchar('\0');
 	}
+	else if (specifier == 'd' || specifier == 'i')
+			count += _putint(va_arg(list, int));
 	else if (specifier == '%')
 		count += _putchar('%');
 	else
-	{
 		count += _putchar(specifier);
-	}
 	return (count);
 }
